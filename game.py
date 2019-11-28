82cbfb3295d78ec3f181d7ad85637bf04ea5187d
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-11-28 19:44:54 +0200

    UnitTest: add skeleton to referee test, NOT passing

---
+++ game.py
@@ -0,0 +1,8 @@
+from collections import namedtuple
+
+
+Game = namedtuple('Game', 'player1 player2')
+
+
+def referee(*args, **kwargs):
+    pass
---
+++ tests.py
@@ -0,0 +1,38 @@
+import unittest
+from itertools import product
+from collections import Counter
+
+from game import referee, Game
+
+
+class MyTestCase(unittest.TestCase):
+    def test_referee(self):
+        """
+        `all_possible_games`: a cartesian gird of all possible plays.
+        | player1 	| player2 	|
+        |:-------:	|:-------:	|
+        | scissor 	| scissor 	|
+        | scissor 	| paper   	|
+        | scissor 	| rock    	|
+        | paper   	| scissor 	|
+        | paper   	| paper   	|
+        | paper   	| rock    	|
+        | rock    	| scissor 	|
+        | rock    	| paper   	|
+        | rock    	| rock    	|
+        If the game was played several times in which each possible
+        play appeared only once, player1 will win 3 times, player2
+        will win 3 times and they will tie three times.
+        """
+        counter = Counter({'player1': 0, 'player2': 0, 'tie': 0})
+        choices = ['scissor', 'paper', 'rock']
+        all_possible_games = product(choices, repeat=2)
+
+        for game in all_possible_games:
+            result = referee(Game(*game))
+            counter[result] += 1
+        self.assertEqual({'player1': 3, 'player2': 3, 'tie': 3}, dict(counter))
+
+
+if __name__ == '__main__':
+    unittest.main()
