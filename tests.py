039126c9935fb545e7d2fe8a4525f18a80c07bb7
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-12-02 22:24:35 +0200

    Move unittest for single round from simuation test class to Referee test class

--- tests.py
+++ tests.py
@@ -1,38 +1,11 @@
 import unittest
 from itertools import product
 
-from game import referee, Game
-from referee import Referee
+from referee import Referee, Round
 
 
 class MyTestCase(unittest.TestCase):
-    def test_referee(self):
-        """
-        `all_possible_games`: a cartesian gird of all possible plays.
-        | player1 	| player2 	|
-        |:-------:	|:-------:	|
-        | scissor 	| scissor 	|
-        | scissor 	| paper   	|
-        | scissor 	| rock    	|
-        | paper   	| scissor 	|
-        | paper   	| paper   	|
-        | paper   	| rock    	|
-        | rock    	| scissor 	|
-        | rock    	| paper   	|
-        | rock    	| rock    	|
-        If the game was played several times in which each possible
-        play appeared only once, player1 will win 3 times, player2
-        will win 3 times and they will tie three times.
-        """
-        counter = {'player1': 0, 'player2': 0, 'tie': 0}
-        choices = ['scissor', 'paper', 'rock']
-        all_possible_games = product(choices, repeat=2)
-
-        for game in all_possible_games:
-            result = referee(Game(*game))
-            counter[result] += 1
-
-        self.assertEqual({'player1': 3, 'player2': 3, 'tie': 3}, counter)
+    pass
 
 
 class TestReferee(unittest.TestCase):
@@ -55,6 +28,34 @@ class TestReferee(unittest.TestCase):
         for comp, expected_result in zip(comps, expected):
             print(comp, expected_result)
             self.assertEqual(expected_result, self.referee(comp))
+
+    def test_referee_judge_single_round(self):
+        """
+        `all_possible_games`: a cartesian gird of all possible plays.
+        | player1 	| player2 	|
+        |:-------:	|:-------:	|
+        | scissor 	| scissor 	|
+        | scissor 	| paper   	|
+        | scissor 	| rock    	|
+        | paper   	| scissor 	|
+        | paper   	| paper   	|
+        | paper   	| rock    	|
+        | rock    	| scissor 	|
+        | rock    	| paper   	|
+        | rock    	| rock    	|
+        If the game was played several times in which each possible
+        play appeared only once, player1 will win 3 times, player2
+        will win 3 times and they will tie three times.
+        """
+        counter = {'player1': 0, 'player2': 0, 'tie': 0}
+        choices = ['scissor', 'paper', 'rock']
+        all_possible_games = product(choices, repeat=2)
+
+        for game in all_possible_games:
+            result = referee(Round(*game))
+            counter[result] += 1
+
+        self.assertEqual({'player1': 3, 'player2': 3, 'tie': 3}, counter)
 
 
 if __name__ == '__main__':
