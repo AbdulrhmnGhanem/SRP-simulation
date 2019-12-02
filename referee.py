023a6764fb62d6b0820fbf4ac55c9b168d066deb
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-12-02 22:19:40 +0200

    Impelement the logic for Referee class

--- referee.py
+++ referee.py
@@ -19,11 +19,20 @@ class Referee:
 
     @judge.register
     def _judge_match(self, match: dict) -> str:
-        pass
+        match.pop('tie')
+        c = collections.Counter(match)
+        most = c.most_common()
+        # return the highest value unless both values are equal returns `tie`
+        return most[0][0] if most[1][1] != most[0][1] else 'tie'
 
     @judge.register
     def _judge_round(self, round_: Round) -> str:
-        pass
+        rules = {'scissor': 'paper', 'paper': 'rock', 'rock': 'scissor'}
+
+        if rules[round_.player1] == round_.player2:
+            return 'player1'
+        else:
+            return 'player2' if round_.player1 != round_.player2 else 'tie'
 
 
 if __name__ == '__main__':
